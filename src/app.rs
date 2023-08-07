92775b4855f299b56b74a438bacbc19330a5844e
Indrazar <110272232+Indrazar@users.noreply.github.com>
2023-08-07 11:39:00 -0400

    update to leptos v0.5.0-beta

--- src/app.rs
+++ src/app.rs
@@ -6,13 +6,11 @@ use leptos_router::*;
 use leptos_axum::redirect;
 
 #[component]
-pub fn App(cx: Scope) -> impl IntoView {
+pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
-    provide_meta_context(cx);
+    provide_meta_context();
 
     view! {
-        cx,
-
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/start-axum.css"/>
@@ -24,8 +22,8 @@ pub fn App(cx: Scope) -> impl IntoView {
         <Router>
             <main>
                 <Routes>
-                    <Route path="" view=|cx| view! { cx, <HomePage/> }/>
-                    <Route path="/page2" view=|cx| view! { cx, <Page2/> }/>
+                    <Route path="" view=|| view! {<HomePage/> }/>
+                    <Route path="/page2" view=|| view! {<Page2/> }/>
                 </Routes>
             </main>
         </Router>
@@ -34,10 +32,10 @@ pub fn App(cx: Scope) -> impl IntoView {
 
 /// Renders the home page of your application.
 #[component]
-fn HomePage(cx: Scope) -> impl IntoView {
-    let go_page2 = create_server_action::<GoPage2>(cx);
+fn HomePage() -> impl IntoView {
+    let go_page2 = create_server_action::<GoPage2>();
 
-    view! { cx,
+    view! {
         <h1>"Welcome to Leptos!"</h1>
         <ActionForm action=go_page2>
             <input type="submit" value="Go to Page2"/>
@@ -47,10 +45,10 @@ fn HomePage(cx: Scope) -> impl IntoView {
 
 /// Renders page2 of your application.
 #[component]
-fn Page2(cx: Scope) -> impl IntoView {
-    let go_home = create_server_action::<GoHome>(cx);
+fn Page2() -> impl IntoView {
+    let go_home = create_server_action::<GoHome>();
 
-    view! { cx,
+    view! {
         <h1>"Welcome to Page2"</h1>
         <ActionForm action=go_home>
             <input type="submit" value="Go to /"/>
@@ -59,13 +57,13 @@ fn Page2(cx: Scope) -> impl IntoView {
 }
 
 #[server(GoHome, "/api")]
-pub async fn go_home(cx: Scope) -> Result<(), ServerFnError> {
-    redirect(cx, "/");
+pub async fn go_home() -> Result<(), ServerFnError> {
+    redirect("/");
     Ok(())
 }
 
 #[server(GoPage2, "/api")]
-pub async fn go_page2(cx: Scope) -> Result<(), ServerFnError> {
-    redirect(cx, "/page2");
+pub async fn go_page2() -> Result<(), ServerFnError> {
+    redirect("/page2");
     Ok(())
 }
--- src/error_template.rs
+++ src/error_template.rs
@@ -7,15 +7,15 @@ use thiserror::Error;
 use leptos_axum::ResponseOptions;
 
 #[derive(Clone, Debug, Error)]
-pub enum AppError {
+pub enum AppPageError {
     #[error("Not Found")]
     NotFound,
 }
 
-impl AppError {
+impl AppPageError {
     pub fn status_code(&self) -> StatusCode {
         match self {
-            AppError::NotFound => StatusCode::NOT_FOUND,
+            AppPageError::NotFound => StatusCode::NOT_FOUND,
         }
     }
 }
@@ -24,12 +24,11 @@ impl AppError {
 // Feel free to do more complicated things here than just displaying the error.
 #[component]
 pub fn ErrorTemplate(
-    cx: Scope,
     #[prop(optional)] outside_errors: Option<Errors>,
     #[prop(optional)] errors: Option<RwSignal<Errors>>,
 ) -> impl IntoView {
     let errors = match outside_errors {
-        Some(e) => create_rw_signal(cx, e),
+        Some(e) => create_rw_signal(e),
         None => match errors {
             Some(e) => e,
             None => panic!("No Errors found and we expected errors!"),
@@ -39,22 +38,22 @@ pub fn ErrorTemplate(
     let errors = errors.get();
 
     // Downcast lets us take a type that implements `std::error::Error`
-    let errors: Vec<AppError> = errors
+    let errors: Vec<AppPageError> = errors
         .into_iter()
-        .filter_map(|(_k, v)| v.downcast_ref::<AppError>().cloned())
+        .filter_map(|(_k, v)| v.downcast_ref::<AppPageError>().cloned())
         .collect();
     println!("Errors: {errors:#?}");
 
     // Only the response code for the first error is actually sent from the server
     // this may be customized by the specific application
     cfg_if! { if #[cfg(feature="ssr")] {
-        let response = use_context::<ResponseOptions>(cx);
+        let response = use_context::<ResponseOptions>();
         if let Some(response) = response {
             response.set_status(errors[0].status_code());
         }
     }}
 
-    view! {cx,
+    view! {
         <h1>{if errors.len() > 1 {"Errors"} else {"Error"}}</h1>
         <For
             // a function that returns the items we're iterating over; a signal is fine
@@ -62,7 +61,7 @@ pub fn ErrorTemplate(
             // a unique key for each item as a reference
             key=|(index, _error)| *index
             // renders each item to a view
-            view= move |cx, error| {
+            view= move |error| {
                 let error_string = error.1.to_string();
                 let error_code= error.1.status_code();
                 view! {
